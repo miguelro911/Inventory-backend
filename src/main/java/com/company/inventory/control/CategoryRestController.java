da7f38c944fb35ee5c620a0c262cdfa96f311edf
miguelro911 <miguel.romo.cac@gmail.com>
2023-03-17 18:39:33 -0500

    Se agrego el crossorigin para que se pueda comunicar con el front

--- src/main/java/com/company/inventory/control/CategoryRestController.java
+++ src/main/java/com/company/inventory/control/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventory.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("api/v1")
 public class CategoryRestController {
